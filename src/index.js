bd0af56dbe216111eb62acbef2a77888198c6035
flangoria <audrey.mabry90@gmail.com>
2024-04-01 12:06:39 -0400

    Added basic form handling and Typewriter plugin

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
-alert("Hello world");
+function generatePoem(event) {
+    event.preventDefault();
+
+
+    new Typewriter("#poem", {
+        strings: "I hunger for your sleek laugh, your hands the color of a savage harvest",
+        autoStart: true,
+        delay: 1,
+        cursor: "",
+      });
+
+}
+
+
+let poemFormElement = document.querySelector("#poem-generator-form");
+poemFormElement.addEventListener("submit", generatePoem);
